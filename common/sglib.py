3fee291a3284b44d3a5234a7d4e17e02cf64e31e
Preston <preston@sudogami.com>
2021-11-16 03:37:56 -0500

    updated .gitignore, refactored sglib.py functions.

--- common/sglib.py
+++ common/sglib.py
@@ -1,7 +1,5 @@
 import os
 from django.conf import settings
-from django.shortcuts import render
-import itertools
 
 path = settings.MEDIA_ROOT
 
@@ -27,6 +25,35 @@ def get_sg_path(fp=""):
     for splits in uf_sg:
         fm_sg.append("/".join(splits.strip("/").split('/')[4:]))
 
-
     return fm_sg
+
+
+def get_images(fp=""):
+    """ Returns all images within a given directory, does not crawl subdirectories."""
+
+    exts = ["jpg", "jpeg", "png", "gif"]
+
+    # concat root and given filepath
+    galfp = os.path.join(path, fp)
+
+    # image names in directory to list
+    prim_gallery = [img for img in os.listdir(galfp) if os.path.isfile(os.path.join(galfp, img)) if img.split(".")[-1] in exts]
+
+    if prim_gallery is None:
+        raise FileNotFoundError(f"Unable to locate files.")
+
+    # unformatted and formatted list declerations
+    uffp_prim = []
+    fmfp_prim = []
+
+    # append media_root to given images
+    for imgs in prim_gallery:
+        uffp_prim.append(os.path.join(galfp, imgs))
+
+    # strip the first 4 folders from the filepath
+    for splits in uffp_prim:
+        fmfp_prim.append("/".join(splits.strip("/").split('/')[4:]))
+
+    return fmfp_prim
+
 
