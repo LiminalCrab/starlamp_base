13168e50423b4132ed7c0832c753acfc14f2104c
Preston <preston@sudogami.com>
2021-11-16 21:20:02 -0500

    get_thumbs finds first image

--- galleries/tests.py
+++ galleries/tests.py
@@ -16,12 +16,13 @@ def get_thumbs():
 
     unformatted_exclusion_list = []
 
+    unformatted_image_files = []
 
     for gdirs in gallery_folder_scan:
         if gdirs.is_dir():
             gallery_folder_names.append(gdirs.name)
 
-    print(f"{len(gallery_folder_names)} galleries discovered: {gallery_folder_names}")
+    print(f"[{len(gallery_folder_names)}] galleries discovered: {gallery_folder_names}")
 
     # join the media_root path to the gallery name.
     for items in gallery_folder_names:
@@ -32,8 +33,21 @@ def get_thumbs():
         for root, subdirectories, files, in os.walk(items, topdown=True, followlinks=False):
             subdirectories[:] = [d for d in subdirectories if d not in items]
             unformatted_exclusion_list.append(subdirectories)
-            
 
+    exclusion_list = list(itertools.chain.from_iterable(unformatted_exclusion_list))
 
+    print(f"[{len(exclusion_list)}] subgalleries excluded from thumbnail search: {exclusion_list}")
+
+    # Retrieve the thumbnails from non-excluded galleries.
+
+    for root, subdirectories, files, in os.walk(path, topdown=True, followlinks=False):
+        for items in exclusion_list:
+            if items in subdirectories:
+                subdirectories.remove(items)
+        for file in sorted(files):
+            unformatted_image_files.append(os.path.join(root, file))
+            break
+
+    
 
 get_thumbs()
