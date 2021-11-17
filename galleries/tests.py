3c76ef431e3223f24b543ed65b0fa1242c926156
Preston <preston@sudogami.com>
2021-11-16 19:43:15 -0500

    merged get_images() and get_models() into get_files() function

--- galleries/tests.py
+++ galleries/tests.py
@@ -3,5 +3,35 @@ from django.conf import settings
 path = settings.MEDIA_ROOT
 
 
-def get_models(fp=""):
-    """ Returns a list of model filepaths within a given directory"""
+def get_files(extensions, fp=""):
+    exts = [extensions]
+
+    # concat root and given filepath
+    galfp = os.path.join(path, fp)
+
+    # image names in directory to list
+    prim_gallery = [img for img in os.listdir(galfp) if os.path.isfile(os.path.join(galfp, img)) if
+                    img.split(".")[-1] in exts]
+
+    if prim_gallery is None:
+        raise FileNotFoundError(f"Unable to locate files.")
+
+    # unformatted and formatted list declerations
+    uffp_prim = []
+    fmfp_prim = []
+
+    # append media_root to given images
+    for imgs in prim_gallery:
+        uffp_prim.append(os.path.join(galfp, imgs))
+
+    # strip the first 4 folders from the filepath
+    for splits in uffp_prim:
+        fmfp_prim.append("/".join(splits.strip("/").split('/')[4:]))
+
+    print(fmfp_prim)
+
+
+get_files("mdl", "models")
+
+
+
