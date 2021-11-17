6b0852203d14f4ad77eba5e6c964b3e9ba768005
Preston <preston@sudogami.com>
2021-11-17 00:19:22 -0500

    models.py reorganized, added images

--- galleries/models.py
+++ galleries/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
-
-class Subgallery(models.Model):
-    parent = models.CharField(128)
-    name = models.CharField(max_length=128)
-    relative_path = models.CharField(248)
-    absolute_path = models.CharField(248)
-
 class Gallery(models.Model):
-    children = models.ForeignKey(Subgallery, on_delete=models.CASCADE, related_name="parent")
+    children = models.CharField(max_length=128)
     name = models.CharField(max_length=128)
     thumbnail = models.CharField(max_length=248)
+    relative_path = models.CharField(max_length=248)
+    absolute_path = models.CharField(max_length=248)
+
+class Subgallery(models.Model):
+    parent = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    relative_path = models.CharField(max_length=248)
+    absolute_path = models.CharField(max_length=248)
+
+
+class Images(models.Model):
+    name = models.CharField(max_length=64)
+    absolute_path = models.CharField(max_length=248)
+    relative_path = models.CharField(max_length=248)
+    upload_date = models.DateField()
+
+    
