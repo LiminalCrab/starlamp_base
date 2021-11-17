59b856bad552f4e9c7780f50c843b9f3c93c3390
Preston <preston@sudogami.com>
2021-11-17 00:29:03 -0500

    changes to relations and foreignkeys

--- galleries/models.py
+++ galleries/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 
+
 class Gallery(models.Model):
-    children = models.CharField(max_length=128)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=64)
     thumbnail = models.CharField(max_length=248)
-    relative_path = models.CharField(max_length=248)
-    absolute_path = models.CharField(max_length=248)
+    children = models.CharField(max_length=128)
+    filepath = models.CharField(max_length=248)
+
 
 class Subgallery(models.Model):
+    name = models.CharField(max_length=64)
+    thumbnail = models.CharField(max_length=248)
     parent = models.ForeignKey(Gallery, on_delete=models.CASCADE)
-    name = models.CharField(max_length=128)
-    relative_path = models.CharField(max_length=248)
-    absolute_path = models.CharField(max_length=248)
+    filepath = models.CharField(max_length=248)
 
 
 class Images(models.Model):
     name = models.CharField(max_length=64)
-    absolute_path = models.CharField(max_length=248)
-    relative_path = models.CharField(max_length=248)
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    subgallery = models.ForeignKey(Subgallery, on_delete=models.CASCADE)
+    filepath = models.CharField(max_length=248)
     upload_date = models.DateField()
 
-    
