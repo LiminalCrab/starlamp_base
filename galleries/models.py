2c985260312267dbfdec41740ad402e844c83f14
Preston <preston@sudogami.com>
2021-11-16 23:33:29 -0500

    database tables Gallery and Subgallery

--- galleries/models.py
+++ galleries/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Subgallery(models.Model):
+    name = models.CharField(max_length=128)
+    parent = models.CharField(128)
+    relative_path = models.CharField(248)
+    absolute_path = models.CharField(248)
+
+class Gallery(models.Model):
+    name = models.CharField(max_length=128)
+    thumbnail = models.CharField(max_length=248)
