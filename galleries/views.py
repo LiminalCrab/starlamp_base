ed7fca5a4fed099c08c2d1869833d193c532d497
Preston <preston@sudogami.com>
2021-11-17 21:11:53 -0500

    views.py, templates dir, base.html, index.html

--- galleries/views.py
+++ galleries/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Gallery
+from .models import Subgallery
+from .models import Images
 
 # Create your views here.
+
+
+def index(req):
+    return render(req, "galleries/index.html", {
+        "galleries": Gallery.objects.all()
+    })
+
