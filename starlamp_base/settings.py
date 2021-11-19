543d54344bae616bd52603a9798d66104b4f34e1
Preston <preston@sudogami.com>
2021-11-18 20:55:42 -0500

    webgl canvas rendering

--- starlamp_base/settings.py
+++ starlamp_base/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "node_modules"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "node_modules"),
+    os.path.join(BASE_DIR, 'node_modules', 'three', 'build')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # USER DEFINED
