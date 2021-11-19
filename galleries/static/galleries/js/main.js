8e3d584d3be0e3bbf42540cbc4487b029214554a
Preston <preston@sudogami.com>
2021-11-18 22:01:34 -0500

    render cube

--- galleries/static/galleries/js/main.js
+++ galleries/static/galleries/js/main.js
@@ -8,7 +8,24 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.setSize(200, 200);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const cube = new THREE.Mesh( geometry, material)
+scene.add( cube );
+
+camera.position.z = 5;
+
+const animate = function () {
+				requestAnimationFrame( animate );
+
+				cube.rotation.x += 0.01;
+				cube.rotation.y += 0.01;
+
+				renderer.render( scene, camera );
+			};
+
+			animate();
 
 renderer.render(scene, camera)
