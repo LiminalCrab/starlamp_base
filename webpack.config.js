e23fbf2a24f94e67ea34f96d8178894803f2b5d9
Preston <preston@sudogami.com>
2021-11-20 02:00:51 -0500

    dangerouslyAllowCleanPatternsOutsideProject

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = [
             ],
         },
         plugins: [
-            new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+            new CleanWebpackPlugin({dangerouslyAllowCleanPatternsOutsideProject: true, dry: false}),
             new MiniCssExtractPlugin(),
             new HTMLWebpackPlugin({
                 template: path.resolve(__dirname, './assets/index.html'),
